83956ba546b44163ffb8cc3ddaeae89ccb8ff790
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-03-14 22:27:25 +0100

    removed comments

--- api/index.js
+++ api/index.js
@@ -1,19 +1,21 @@
-const express = require("express"); //import express library
-const app = express(); // create new instance for the express application
-const dotenv = require("dotenv"); // import dotenv
-const mongoose = require("mongoose") // imports mongoose library which is useful for interacting with a MongoDb databse
-const authRoute = require("./routes/auth") // This module contains definitions for authetication-related functionalities
+const express = require("express"); 
+const app = express();
+port = 5000
+const dotenv = require("dotenv"); 
+const mongoose = require("mongoose") 
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 
 dotenv.config();
 
-app.use(express.json()) // This middleware function handles requests with JSON data in the requests body.
-
+app.use(express.json()) 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connected!'))
-  .catch((err) => console.log(err)); // connects to MongoDb database
+  .catch((err) => console.log(err)); 
 
-app.use("/api/auth", authRoute) // any routes defines in the authRoute will be accessed with the /api/auth prefix
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute) 
 
-app.listen("5000", () => {
+app.listen(port, () => {
   console.log("Backend is running");
-}); // Exprss application starts listening for incoming requests at port 5000
+}); 
