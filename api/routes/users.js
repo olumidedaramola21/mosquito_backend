2a70b7d978b46aa64ef8afbe2f45b7f83e45097e
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-03-15 23:11:20 +0100

    delete route

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
-
 // UPDATE
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
@@ -26,12 +25,24 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-
-
-
+// DELETE ai delete hard to debug
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({username: user.username})
+       await User.findByIdAndDelete(req.params.id);
+       res.status(200).json("User has been deleted")
+      } catch (err) {
+        res.status(500).json({error: "unknown"});
+      }
+    } catch (err) {
+      res.status(400).json("user not found!");
+    }
+  } else {
+    res.status(401).json("You can only delete your account!");
+  }
+});
 
 module.exports = router;
-
-
-
-
