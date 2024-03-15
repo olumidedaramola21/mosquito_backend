0ea881911e16f1b4b72795ad007e024e8fea9551
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-03-16 00:50:07 +0100

    add postRoute and use middleware

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose") 
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute) 
+app.use("/api/posts", postRoute) 
 
 app.listen(port, () => {
   console.log("Backend is running");
