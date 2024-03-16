eb555ab1366186646714dfb6f4d70b564eb292fc
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-03-16 12:15:17 +0100

    delete post route updated

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -43,7 +43,7 @@ router.delete("/:id", async (req, res) => {
     if (post.username === req.body.username) {
       try {
         await post.deleteOne();
-        res.status(200).json({ post });
+        res.status(200).json("Post has been deleted");
       } catch (err) {
         res.status(500).json(err);
       }
