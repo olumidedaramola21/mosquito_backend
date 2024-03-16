8fa25bfe529de20e4ae349685261383653a4954c
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-03-16 13:59:13 +0100

    get all post route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
+const Category = require("../models/category");
 
 // CREATE POST
 router.post("/", async (req, res) => {
@@ -59,13 +60,35 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-   
-    res.status(200).json(post)
+
+    res.status(200).json(post);
     if (!post) {
-      return res.status(404).json({error: "Post not found"});
+      return res.status(404).json({ error: "Post not found" });
     }
   } catch (err) {
-    res.status(500).json({ error: "Internal server error" })
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
+
+// GET ALL POSTS
+router.get("/", async (req, res) => {
+  const username = req.query.user; // extracts query parameter from the request url
+  const catname = req.query.cat;
+
+  try {
+    let posts; // stores posts retrieved from db
+    if (username) {
+      posts = await Post.find({username})
+    }   else if (catname) {
+      posts = await Post.find({categories: {
+        $in:[catName] // finds all post where the categories array containds the provided category
+      }})
+    } else {
+      posts = await Post.find()
+    }
+    res.status(200).json(posts)
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 module.exports = router;
